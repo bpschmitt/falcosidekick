90e1ec893d6fd45f4cc42f0baf701836808d135a
bschmitt <bschmitt@newrelic.com>
2023-04-18 14:01:06 -0500

    debug container image

--- outputs/newrelic.go
+++ outputs/newrelic.go
@@ -31,7 +31,7 @@ type newRelicPayload struct {
 // Parse K8s attributes
 func parseK8sAttributes(src string) map[string]string {
 	//log.Println("parseK8sAttributes: " + src)
-	var rex = regexp.MustCompile("([A-Za-z0-9\\-\\_\\.]+)=([A-Za-z0-9\\-\\_/]+)")
+	var rex = regexp.MustCompile("([A-Za-z0-9\\-\\_\\.]+)=([A-Za-z0-9\\-\\_/\\.]+)")
 	data := rex.FindAllStringSubmatch(src, -1)
 
 	res := make(map[string]string)
@@ -40,6 +40,8 @@ func parseK8sAttributes(src string) map[string]string {
 		v := kv[2]
 		res[k] = v
 	}
+
+	log.Println("image: " + res[`image`])
 	//log.Println("k8s.pod: " + res[`k8s.pod`])
 	return res
 }
